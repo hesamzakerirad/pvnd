1aef93edbf7793337f445f46f44ab5f0af65ab10
Hesam Rad <hesamzakerirad@gmail.com>
2026-02-22 11:58:32 +0330

    feat: add change log page

--- database/seeders/ChangeLogSeeder.php
+++ database/seeders/ChangeLogSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ChangeLogSeeder extends Seeder
 {
@@ -11,6 +13,19 @@ class ChangeLogSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $data = [
+            [
+                'content' => 'جرقه ایده سرویس پیوند در ذهن من ([حسام راد](https://hesamrad.com))',
+                'created_at' => Carbon::create(2025, 3, 22),
+                'updated_at' => Carbon::create(2025, 3, 22),
+            ],
+            [
+                'content' => 'شروع ایده‌پردازی و توسعه محصول',
+                'created_at' => Carbon::create(2026, 2, 3),
+                'updated_at' => Carbon::create(2026, 2, 3),
+            ],
+        ];
+
+        DB::table('change_logs')->insert($data);
     }
 }
--- resources/views/change-logs.blade.php
+++ resources/views/change-logs.blade.php
@@ -1,26 +1,76 @@
 @extends('layout.master')
 
 @section('content')
-    <div class="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-        <div aria-hidden="true" class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
-            <div style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-                class="relative left-1/2 -z-10 aspect-1155/678 w-144.5 max-w-none -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-288.75">
+    <div class="isolate bg-gray-50 pt-18 sm:pt-24">
+        <div class="py-36">
+            <div class="mx-auto max-w-5xl">
+                <div class="flex gap-3 justify-start align-middle">
+                    <div class="flex flex-col justify-center items-start">
+                        <h1 class="text-3xl sm:text-3xl font-bold tracking-tight text-balance text-gray-900 mb-6">
+                            مسیر رشد پیوند
+                        </h1>
+                        <div class="mb-6">
+                            <p class="font-normal">در این برگه روند رشد پیوند به عنوان یک محصول رو از ابتدای تولد تا امروز
+                                مشاهده می‌کنید.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="mx-auto max-w-2xl text-center">
-            <h2 class="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl">مسیر رشد</h2>
-            <p class="mt-2 text-lg/8 text-gray-600">در این برگه می‌توانید مسیر رشد پیوند را از ابتدای ساخت تا این لحظه مشاهده کنید.</p>
-        </div>
-        <div>
-            <ul class="list-inside list-disc marker:text-blue-600">
-                @foreach ($changeLogs as $log)
-                    <li class="mb-2 flex">
-                        <span class="font-semibold">{{ $log->created_at->format('Y/m/d') }}</span>
-                        <span class="mx-3"> - </span>
-                        <span>@markdown($log->content)</span>
-                    </li>
-                @endforeach
-            </ul>
+
+        <div class="bg-white">
+            <div class="mx-auto max-w-5xl py-12">
+                <div>
+                    <ul class="list-inside list-disc marker:text-blue-600">
+                        @forelse ($changeLogs as $log)
+                            <li class="mb-2 flex">
+                                <span class="font-semibold">{{ $log->created_at->format('Y/m/d') }}</span>
+                                <span class="mx-3"> - </span>
+                                <span>@markdown($log->content)</span>
+                            </li>
+                        @empty
+                            پیوند هنوز هیچ رشدی نکرده... باید کمی صبر کنید!
+                        @endforelse
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
+
+    @if (!empty($faq))
+        <section class="py-10 sm:py-16 lg:py-24">
+            <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
+                <div class="max-w-2xl mx-auto text-center">
+                    <h2 class="text-3xl sm:text-4xl font-bold leading-tight text-black">
+                        پرسش‌های پرتکرار
+                    </h2>
+                </div>
+                <div class="max-w-7xl mx-auto mt-8 space-y-4 md:mt-16">
+                    @foreach ($faq as $key => $item)
+                        @php
+                            $key++;
+                        @endphp
+                        <div
+                            class="transition-all duration-200 bg-white border border-gray-50 rounded-xl shadow-lg cursor-pointer hover:bg-gray-50">
+                            <button type="button" id="question{{ $key }}" data-state="closed"
+                                class="flex items-center justify-between w-full px-4 py-5 sm:p-6">
+                                <span class="flex text-lg font-semibold text-black">
+                                    @markdown($item->question)
+                                </span>
+                                <svg id="arrow{{ $key }}" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6 text-gray-400">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M19 9l-7 7-7-7">
+                                    </path>
+                                </svg>
+                            </button>
+                            <div id="answer{{ $key }}" style="display:none"
+                                class="px-4 pb-5 sm:px-6 sm:pb-6 text-gray-500">
+                                @markdown($item->answer)
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+        </section>
+    @endif
 @endsection
